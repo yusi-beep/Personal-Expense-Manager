0d4109bc7f9f236142c635635f40598efb5a6f8e
Yusuf Yusufov (Yosaka) <yusitoo1234@gmail.com>
2025-08-26 09:46:59 +0300

    feat: Adding record

--- pet.py
+++ pet.py
@@ -10,6 +10,18 @@ if not os.path.exists(FILE_NAME):
 		writer = csv.writer(file)
 		writer.writerow(["date", "type", "category", "amount", "description"])
 
+def add_record():
+	date = datetime.now().strftime("%Y-%m-%d")
+	entry_type = input("Type (expense/income): ").strip().lower()
+	category = input("Category:" ).strip()
+	amount = float(input("Amount: "))
+	description = input("Description: " ).strip()
+
+	with open(FILE_NAME,"a", newline="", encoding="utf-8") as file:
+		writer = csv.writer(file)
+		writer.writerow([date, entry_type, category, amount, description])
+
+	print("Record added successfully!")
 
 def menu():
 	while True:
@@ -18,7 +30,16 @@ def menu():
 		print("2. View record")
 		print("3. Balance")
 		print("4. Exit")
-		break
+
+		choice = input("Select: ")
+
+		if choice == "1":
+			add_record()
+		elif choice == "4":
+			print("Exit from program successfully.")
+			break
+		else:
+			print("Invalid selection!")
 
 if __name__ == "__main__":
 	menu()
